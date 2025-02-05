d4b23cf46754dcc66099bd48fc9a340f3385dc91
ThembaMtshelwane <thembamm3@gmail.com>
2025-02-05 12:12:59 +0200

    Error middleware set up

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || "9000";
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
