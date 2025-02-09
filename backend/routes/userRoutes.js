d797c518e1d4d1b988238c01c4ab62d82406fa65
ThembaMtshelwane <thembamm3@gmail.com>
2025-02-09 18:47:23 +0200

    Minor changes: change the add user router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.route("/").post(registerAllUsers).get(fetchAllUsers);
 router.post("/admin", createAdminUser);
 router.post("/login", loginUser);
-router.post("/register", addUser);
+router.post("/add-user", addUser);
 router.route("/:id").get(fetchUserById).delete(deleteUser);
 
 export default router;
