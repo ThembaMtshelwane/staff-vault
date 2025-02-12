317a5d62039444aa6118ecb8f959882dfe5aa605
ThembaMtshelwane <thembamm3@gmail.com>
2025-02-12 16:06:33 +0200

    Admin layout: Toggle sidebar

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -5,19 +5,13 @@ import { FaRegUser } from "react-icons/fa";
 import { FiSettings } from "react-icons/fi";
 import { Outlet } from "react-router";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AdminLayout = () => {
-  const [toggle, setToggle] = useState(true);
   return (
     <section className="bg-background h-screen relative">
       {/* <Navbar /> */}
-
-      {toggle ? (
-        <div onClick={() => setToggle((prev) => !prev)}>lip</div>
-      ) : (
-        <SideMenu />
-      )}
+      <SideMenu />
 
       <section className="border">
         <Outlet />
@@ -29,25 +23,55 @@ const AdminLayout = () => {
 export default AdminLayout;
 
 const SideMenu = () => {
+  const [toggle, setToggle] = useState(false);
+  const menuRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setToggle(false);
+      }
+    };
+
+    if (toggle) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [toggle]);
   return (
-    <section className="border border-red-600 w-fit p-2 bg-secondary text-background absolute left-0 transform translate-y-[25%]">
-      <ul className="border border-blue-400 flex flex-col gap-4 ">
-        <li className="icon-container">
-          <MdOutlineDashboard className="sidebar-icons" />
-        </li>
-        <li className="icon-container">
-          <GrGroup className="sidebar-icons" />
-        </li>
-        <li className="icon-container">
-          <PiTreeStructureLight className="sidebar-icons rotate-90 font-extrabold" />
-        </li>
-        <li className="icon-container">
-          <FaRegUser className="sidebar-icons" />
-        </li>
-        <li className="icon-container">
-          <FiSettings className="sidebar-icons" />
-        </li>
-      </ul>
-    </section>
+    <>
+      <section
+        className={`w-fit py-4 px-2 bg-secondary text-background absolute left-0 transform translate-y-[25%] rounded-tr-2xl rounded-br-2xl
+        }`}
+      >
+        {toggle ? (
+          <ul ref={menuRef} className="flex flex-col gap-4 ">
+            <li className="icon-container">
+              <MdOutlineDashboard className="sidebar-icons" />
+            </li>
+            <li className="icon-container">
+              <GrGroup className="sidebar-icons" />
+            </li>
+            <li className="icon-container">
+              <PiTreeStructureLight className="sidebar-icons rotate-90 font-extrabold" />
+            </li>
+            <li className="icon-container">
+              <FaRegUser className="sidebar-icons" />
+            </li>
+            <li className="icon-container">
+              <FiSettings className="sidebar-icons" />
+            </li>
+          </ul>
+        ) : (
+          <div
+            onClick={() => setToggle(true)}
+            className="h-[55vh] w-[10px] "
+          ></div>
+        )}
+      </section>
+    </>
   );
 };
