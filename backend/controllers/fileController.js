b9195e45580d1f307555d2a7ded4695e32bda300
ThembaMtshelwane <thembamm3@gmail.com>
2025-03-19 11:21:50 +0200

    File service: Delete file service

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -1,11 +1,14 @@
 // Controller for handling file operations
 import File from "../model/fileUploadModel.js";
 import expressAsyncHandler from "express-async-handler";
-import path from "path";
-import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "../constants/http.codes.js";
+import { NOT_FOUND } from "../constants/http.codes.js";
 import HTTP_Error from "../utils/httpError.js";
 import { fetchDocs } from "../service/crudHandlerFactory.js";
-import { fileUploadService } from "../service/fileService.js";
+import {
+  fileDeleteService,
+  fileUploadService,
+  getFilteredFilesService,
+} from "../service/fileService.js";
 
 /**
  * Uploads a file to GridFS and stores its metadata.
@@ -15,14 +18,10 @@ import { fileUploadService } from "../service/fileService.js";
 export const uploadFile = expressAsyncHandler(async (req, res) => {
   const uploaded = await fileUploadService(req.file, req.body);
 
-  if (!uploaded) {
-    throw new HTTP_Error("Failed to upload file", INTERNAL_SERVER_ERROR);
-  }
-
   res.status(201).json({
     success: true,
     message: "File Uploaded Successfully",
-    data: req.file,
+    data: uploaded,
   });
 });
 
@@ -44,13 +43,7 @@ export const downloadFile = expressAsyncHandler(async (req, res) => {
 export const getAllFiles = fetchDocs(File);
 
 export const getFilteredFiles = expressAsyncHandler(async (req, res) => {
-  const documentType = req.query.documentType;
-
-  const files = await File.find({ documentType }).exec();
-
-  if (!files) {
-    throw new HTTP_Error("HTTP_Error fetching file", INTERNAL_SERVER_ERROR);
-  }
+  const { files, documentType } = await getFilteredFilesService(req.query);
   res.status(200).json({
     success: true,
     message: `${files.length} ${documentType} files returned`,
@@ -59,18 +52,7 @@ export const getFilteredFiles = expressAsyncHandler(async (req, res) => {
 });
 
 export const deleteFile = expressAsyncHandler(async (req, res) => {
-  const { filename, documentType } = req.params;
-
-  const fileExists = await File.findOne({
-    name: filename,
-    documentType,
-  });
-
-  if (!fileExists) {
-    throw new HTTP_Error("No file found", NOT_FOUND);
-  }
-
-  const file = await File.findByIdAndDelete(fileExists._id);
+  const file = await fileDeleteService(req.params);
   res.status(200).json({
     success: true,
     message: "File deleted successfully",
--- backend/service/fileService.js
+++ backend/service/fileService.js
@@ -1,5 +1,7 @@
+import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "../constants/http.codes.js";
 import File from "../model/fileUploadModel.js";
 import path from "path";
+import HTTP_Error from "../utils/httpError.js";
 
 export const fileUploadService = async (fileData, reqBody) => {
   const relativePath = path.relative(process.cwd(), fileData.path);
@@ -11,6 +13,11 @@ export const fileUploadService = async (fileData, reqBody) => {
     documentType: reqBody.documentType,
   });
   const uploaded = await newFile.save();
+
+  if (!uploaded) {
+    throw new HTTP_Error("Failed to upload file", INTERNAL_SERVER_ERROR);
+  }
+
   return uploaded;
 };
 
@@ -25,4 +32,36 @@ export const fileDownloadService = async (fileData) => {
   const fullPath = path.join(process.cwd(), file.path);
 
   return { fullPath, filename };
+};
+
+export const getFilteredFilesService = async (fileData) => {
+  const documentType = fileData.documentType;
+
+  const files = await File.find({ documentType }).exec();
+
+  if (!files) {
+    throw new HTTP_Error("HTTP_Error fetching file", INTERNAL_SERVER_ERROR);
+  }
+
+  return { files, documentType };
+};
+
+export const fileDeleteService = async (fileData) => {
+  const { filename, documentType } = fileData;
+
+  const fileExists = await File.findOne({
+    name: filename,
+    documentType,
+  });
+
+  if (!fileExists) {
+    throw new HTTP_Error("No file found", NOT_FOUND);
+  }
+
+  const file = await File.findByIdAndDelete(fileExists._id);
+
+  if (!file) {
+    throw new HTTP_Error("Error deleting file", INTERNAL_SERVER_ERROR);
+  }
+  return file;
 };
