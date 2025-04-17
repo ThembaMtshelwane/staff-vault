ad249b681a771604469bfebf5cb90ddea7bee070
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-17 15:05:36 +0200

    Automatic logout wjem access token expires

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -1,4 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
+// import {AppDispatch } from '../store'
+import { AppDispatch } from "../store";
+import { clearCredentials } from "./authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "",
@@ -6,8 +15,30 @@ const baseQuery = fetchBaseQuery({
   credentials: "include",
 });
 
+const customBaseQuery: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  // Catch 401 errors
+  if (result.error && result.error.status === 401) {
+    console.warn("Unauthorized: Logging out and redirecting...");
+
+    // Dispatch logout action
+    (api.dispatch as AppDispatch)(clearCredentials());
+
+    // Optional: redirect using window.location or a custom function
+    window.location.href = "/login"; // or "/"
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery,
+  // baseQuery,
+  baseQuery: customBaseQuery,
   tagTypes: ["User", "Department", "Organization", "File"],
   endpoints: () => ({}),
 });
